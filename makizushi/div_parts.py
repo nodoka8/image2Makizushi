932f8a010ef8526b9441969714bc7bf88d49f85d
tonnurabanzai <61648852+tonnurabanzai@users.noreply.github.com>
2021-11-27 17:22:37 +0900

    csv編集
    画像の名前にした

--- makizushi/div_parts.py
+++ makizushi/div_parts.py
@@ -7,6 +7,7 @@ import csv
 imagename = "me"
 #画像の読み込み
 image = Image.open(imagename + ".png")
+print(type(image))
 
 #-------csv書き込み--------
 def csv_w(row, mode):
@@ -22,12 +23,20 @@ def find_contours(imagename, mode):
     cv2.imwrite("2color.png", img_2)
     #輪郭抽出
     contours, hierarchy = cv2.findContours(img_2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #print(len(labels))
     contours_list = list(contours)
     hierarchy_list = list(hierarchy[0])
+    #labels_list = list(labels[0])
     for i in reversed(range(len(contours_list))):
         if cv2.contourArea(contours_list[i]) < 30:
             contours_list.pop(i)
             hierarchy_list.pop(i)
+            #for j in range(len(contours_list)):
+                #if hierarchy_list[j][2] == i:
+                    #hierarchy_list[j][2] = hierarchy_list[i][2]
+                #if hierarchy_list[j][3] == i:
+                    #hierarchy_list[j][3] = 
+            #labels_list.pop(i)
     return [contours_list, hierarchy_list]
 
 #図形近似
@@ -36,10 +45,10 @@ def app_shape(imagename):
     num_cnts = len(contours)
     matches = [0] * num_cnts
     shape = [0,0,0,0]
-    circle_contours = find_contours("circle", cv2.THRESH_OTSU)[0]
-    square_contours = find_contours("square", cv2.THRESH_OTSU)[0]
-    half_circle_contours = find_contours("half_circle", cv2.THRESH_OTSU)[0]
-    line_contours = find_contours("line", cv2.THRESH_OTSU)[0]
+    circle_contours = find_contours("base_shape/circle", cv2.THRESH_OTSU)[0]
+    square_contours = find_contours("base_shape/square", cv2.THRESH_OTSU)[0]
+    half_circle_contours = find_contours("base_shape/half_circle", cv2.THRESH_OTSU)[0]
+    line_contours = find_contours("base_shape/line", cv2.THRESH_OTSU)[0]
     for i in range(len(matches)):
         shape[0] = cv2.matchShapes(contours[i], circle_contours[0], cv2.CONTOURS_MATCH_I1, 0)
         shape[1] = cv2.matchShapes(contours[i], square_contours[0], cv2.CONTOURS_MATCH_I1, 0)
@@ -54,7 +63,6 @@ def image_numberdraw(imagename):
     for i in range(len(contours)):
         # 輪郭データをタプルに
         area = tuple(map(lambda c: tuple(c[0]), contours[i].tolist()))
-        print(len(contours))
         #インデックスカラーに塗り潰し
         ImageDraw.Draw(copy).polygon(area, fill=(i, 0, 0))
     copy.save(imagename + '_numbercolor.png')
@@ -63,7 +71,7 @@ def image_numberdraw(imagename):
 def calc_rice(imagename):
     contours = find_contours(imagename, cv2.THRESH_BINARY)[0]
     hierarchy = find_contours(imagename, cv2.THRESH_BINARY)[1]
-    rice = []
+    rice = [0]*len(contours)
     for i in range(len(contours)):
         rice[i] = cv2.contourArea(contours[i])
         if hierarchy[i][3] != -1:
@@ -90,18 +98,9 @@ def div_parts(imagename):
         ], area, [image.width, image.height, 0, 0])
         bbox = [box[0]-5, box[1]-5, box[2]+5, box[3]+5]
         output = copy.crop(bbox)
-        output.save(imagename + str(i) + '.png')
-        csv_w([i, output, matches[i], hierarchy[i][3], rice[i]], 'a')
-
-def calc_rice(imagename):
-    contours = find_contours(imagename, cv2.THRESH_BINARY)[0]
-    hierarchy = find_contours(imagename, cv2.THRESH_BINARY)[1]
-    rice = [0, 0, 0, 0]
-    for i in range(len(contours)):
-        rice[i] = cv2.contourArea(contours[i])
-        if hierarchy[i][3] != -1:
-            rice[hierarchy[i][3]] = rice[hierarchy[i][3]] - rice[i]
-    return rice
+        image_name = imagename + str(i) + '.png'
+        output.save(image_name)
+        csv_w([i, image_name, matches[i], hierarchy[i][3], hierarchy[i][2], rice[i]], 'a')
 
 image_numberdraw(imagename)       
 div_parts(imagename)
