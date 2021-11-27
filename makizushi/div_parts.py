e9a3bd11fc97ad37377e5632da3439d1688b9e53
tonnurabanzai <61648852+tonnurabanzai@users.noreply.github.com>
2021-11-27 19:19:50 +0900

    csv編集
    csv完成(仮)

--- makizushi/div_parts.py
+++ makizushi/div_parts.py
@@ -19,7 +19,6 @@ def find_contours(imagename, mode):
     img = cv2.cvtColor(cv2.imread(imagename + ".png"), cv2.COLOR_BGR2GRAY)
     #色を反転させて2値化
     _, img_2 = cv2.threshold(img, 128, 255, mode)
-    cv2.imwrite("2color.png", img_2)
     #輪郭抽出
     contours, hierarchy = cv2.findContours(img_2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #print(len(labels))
@@ -40,8 +39,7 @@ def find_contours(imagename, mode):
     return [contours_list, hierarchy_list]
 
 #図形近似
-def app_shape(imagename):
-    contours = find_contours(imagename, cv2.THRESH_BINARY)[0]
+def app_shape(contours):
     num_cnts = len(contours)
     matches = [0] * num_cnts
     shape = [0,0,0,0]
@@ -57,26 +55,26 @@ def app_shape(imagename):
         matches[i] = shape.index(min(shape))
     return matches
 
-def image_numberdraw(imagename):
-    contours = find_contours(imagename, cv2.THRESH_BINARY)[0]
-    copy = image.copy()
+def image_numberdraw(parts_imagename):
+    parts_image = Image.open("parts_image/" + parts_imagename + ".png")
+    contours = find_contours("parts_image/" + parts_imagename, cv2.THRESH_BINARY)[0]
     for i in range(len(contours)):
         # 輪郭データをタプルに
-        area = tuple(map(lambda c: tuple(c[0]), contours[i].tolist()))
+        area = tuple(map(lambda c: tuple(c[0]), contours[i]))
         #インデックスカラーに塗り潰し
-        ImageDraw.Draw(copy).polygon(area, fill=(i, 0, 0))
-    copy.save(imagename + '_numbercolor.png')
-    csv_w([copy], 'w')
+        ImageDraw.Draw(parts_image).polygon(area, fill=(i, 0, 0))
+    parts_image.save("IDcolor_image/" + parts_imagename + '_IDcolor.png')
+    return parts_imagename + '_IDcolor.png'
 
-def calc_rice(imagename):
-    contours = find_contours(imagename, cv2.THRESH_BINARY)[0]
-    hierarchy = find_contours(imagename, cv2.THRESH_BINARY)[1]
+def calc_size(contours, hierarchy):
     rice = [0]*len(contours)
+    nori = [0]*len(contours)
     for i in range(len(contours)):
         rice[i] = cv2.contourArea(contours[i])
+        nori[i] = cv2.arcLength(contours[i], True)
         if hierarchy[i][3] != -1:
             rice[hierarchy[i][3]] = rice[hierarchy[i][3]] - rice[i]
-    return rice
+    return [rice, nori]
 
 def count_child(n, hierarchy):
     count = 0
@@ -88,11 +86,12 @@ def count_child(n, hierarchy):
 def div_parts(imagename):
     contours = find_contours(imagename, cv2.THRESH_BINARY)[0]
     hierarchy = find_contours(imagename, cv2.THRESH_BINARY)[1]
-    matches = app_shape(imagename)
-    rice = calc_rice(imagename)
+    matches = app_shape(contours)
+    size_rice = calc_size(contours, hierarchy)[0]
+    size_nori = calc_size(contours, hierarchy)[1]
     for i in range(len(contours)):
         # 輪郭データをタプルに
-        area = tuple(map(lambda c: tuple(c[0]), contours[i].tolist()))
+        area = tuple(map(lambda c: tuple(c[0]), contours[i]))
         # マスク画像
         mask = Image.new("L", image.size, 0)
         ImageDraw.Draw(mask).polygon(area, fill=255)
@@ -103,15 +102,14 @@ def div_parts(imagename):
             min(p[0], c[0]), min(p[1], c[1]),
             max(p[2], c[0]), max(p[3], c[1])
         ], area, [image.width, image.height, 0, 0])
-        print(area[i][0])
         bbox = [box[0]-5, box[1]-5, box[2]+5, box[3]+5]
         output = copy.crop(bbox)
-        image_name = imagename + str(i) + '.png'
-        output.save(image_name)
-        csv_w([i, image_name, matches[i], hierarchy[i][2], hierarchy[i][3], image.getpixel(area[i]), count_child(i, hierarchy), rice[i]], 'a')
+        image_name = imagename + str(i)
+        image_name_numberdraw = image_numberdraw(image_name)
+        output.save("parts_image/" + image_name + '.png')
+        csv_w([i, image_name + ".png", image_name_numberdraw, matches[i], hierarchy[i][2], hierarchy[i][3], image.getpixel(area[i]), count_child(i, hierarchy), size_rice[i], size_nori[i]], 'a')
 
-csv_w(["パーツ番号", "パーツ画像", "-1だったら独立パーツ", "親パーツ番号", "パーツカラー", "内包パーツ数", "ご飯の量(パーツサイズ)"], "w")
-#image_numberdraw(imagename)       
+csv_w(["パーツ番号", "パーツ画像", "ID変換画像", "-1だったら独立パーツ", "親パーツ番号", "パーツカラー", "内包パーツ数", "ご飯の量", "海苔のサイズ"], "w")      
 div_parts(imagename)
 
 
