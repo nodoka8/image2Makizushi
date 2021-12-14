02d38f57332752847f8b6b3256c1f2dc534fb1d1
nodoka8 <35965827+nodoka8@users.noreply.github.com>
2021-12-14 17:22:39 +0900

    tanomu

--- Image2Makizushi/Assets/Script/CSVLoader.cs
+++ Image2Makizushi/Assets/Script/CSVLoader.cs
@@ -4,15 +4,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System;
+using UnityEngine.UI;
 
 public class CSVLoader : MonoBehaviour
 {
-
+    [SerializeField]
 	public GuideData guideData;
 
 	public TextAsset csvFile; // CSVファイル
 	public List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
     private string path;
+
+    public Image image;
+    public RawImage img;
 
     // Use this for initialization
     void Start()
@@ -51,17 +55,125 @@ public class CSVLoader : MonoBehaviour
         {
             using (StreamReader sr = new StreamReader(fi.OpenRead(), Encoding.UTF8))
             {
-                string readTxt = sr.ReadToEnd();
-                string[] arr = readTxt.Split(',');
+              //  string readTxt = sr.ReadToEnd();
+                //string[] arr = readTxt.Split(',');
 
-                guideData.ind_shapeid.Add(int.Parse(arr[1]));
-                guideData.parent_num.Add(int.Parse(arr[2]));
-                Debug.Log(guideData.ind_shapeid);
+
+                string line;
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                int itr = 1;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] arr = line.Split(',');
+
+                    if (itr != 1)
+                    {
+
+                            for (int i = 0; i < 13; i++)
+                            {
+                                Debug.Log(arr[i]);
+                            }
+                            if (int.Parse(arr[10]) == 0) {
+                            Debug.Log("texture");
+
+                                guideData.ind_id.Add(int.Parse(arr[0]));
+                                guideData.ind_shapeid.Add(int.Parse(arr[3]));
+                                /*Color color = new Color(int.Parse(arr[6]), int.Parse(arr[7]), int.Parse(arr[8]), int.Parse(arr[9]));
+                                guideData.ind_color.Add(color);*/
+                                guideData.ind_noriwidth.Add(float.Parse(arr[11]));
+                                guideData.ind_noriheight.Add(float.Parse(arr[12]));
+
+                                Texture2D tex = readByBinary(readPngFile(Application.dataPath + "/parts_image/" + arr[1]));
+                                guideData.ind_image.Add(tex);
+                            //image.sprite = null;
+                                image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+                            //image.overrideSprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero); 
+                            //Debug.Log(tex);
+                                //img.texture = tex;
+                            }
+                            else
+                            {
+                                guideData.parent_id.Add(int.Parse(arr[0]));
+                                guideData.parent_num.Add(int.Parse(arr[7]));
+                                guideData.parent_noriwidth.Add(float.Parse(arr[11]));
+                                guideData.parent_noriheight.Add(float.Parse(arr[12]));
+
+                                Texture2D tex = readByBinary(readPngFile(Application.dataPath + "/parts_image/" + arr[1]));
+                                guideData.parent_image.Add(tex);
+
+                                Texture2D tex2 = readByBinary(readPngFile(Application.dataPath + "/IDcolor_image/" + arr[2]));
+                                guideData.parent_idimage.Add(tex2);
+                               // texserch(tex2);
+                        }
+
+                    }
+                    else
+                    {
+                        for (int i = 0; i < arr.Length; i++)
+                        {
+
+                            Debug.Log(arr[i]);
+                        }
+                    }
+
+
+
+                    itr++;
+                }
+
+                //guideData.ind_shapeid.Add(int.Parse(arr[1]));
+                //guideData.parent_num.Add(int.Parse(arr[2]));
+
             }
         }
         catch (Exception e)
         {
             Debug.Log(e);
+        }
+    }
+
+    public byte[] readPngFile(string path)
+    {
+        using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+        {
+            BinaryReader bin = new BinaryReader(fileStream);
+            byte[] values = bin.ReadBytes((int)bin.BaseStream.Length);
+            bin.Close();
+            return values;
+        }
+    }
+
+    public Texture2D readByBinary(byte[] bytes)
+    {
+        Texture2D texture = new Texture2D(1, 1);
+        texture.LoadImage(bytes);
+        return texture;
+    }
+
+    void texserch(Texture2D tex)
+    {
+        Color color;
+        int width = tex.width;
+        int height = tex.height;
+
+        Color[] pix = tex.GetPixels();
+       // Color[] pix = null;
+
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+
+                color = pix[(width * y) + x];
+                //color = new Color() ;
+                if (color.r != 0)
+                {
+                    Debug.Log("p1");
+                }
+
+            }
         }
     }
 
