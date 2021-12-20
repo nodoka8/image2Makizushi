4bf0a9a0776766bbb997e1ab39d6a721bcef8cf5
tonnurabanzai <61648852+tonnurabanzai@users.noreply.github.com>
2021-12-20 12:17:54 +0900

    完成形(仮)
    完成形(仮)

--- makizushi/reset.py
+++ makizushi/reset.py
@@ -1,9 +1,6 @@
 import os
 import shutil
 
-shutil.rmtree("IDcolor_image")
-os.mkdir("IDcolor_image")
-
 shutil.rmtree("parts_image")
 os.mkdir("parts_image")
 
@@ -12,3 +9,7 @@ os.mkdir("line_image")
 
 shutil.rmtree("resipi_line_image")
 os.mkdir("resipi_line_image")
+
+shutil.rmtree("line_image_a")
+os.mkdir("line_image_a")
+
